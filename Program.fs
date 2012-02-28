323d3d1d17269169b503ae461f9f742df69b09c3
dzamkov <dzamkov@xiatek.org>
2012-02-28 14:50:31 -0500

    Black window, it's a start

---
+++ Program.fs
@@ -0,0 +1,5 @@
+﻿module BattleSoup.Program
+
+open BattleSoup.Window
+
+(Window ()).Run ()
---
+++ Window.fs
@@ -0,0 +1,16 @@
+﻿module BattleSoup.Window
+
+open OpenTK
+open OpenTK.Graphics
+open OpenTK.Graphics.OpenGL
+
+/// Main program window.
+type Window () =
+    inherit GameWindow (640, 480, GraphicsMode.Default, "BattleSoup")
+
+    override this.OnRenderFrame args =
+        GL.Clear ClearBufferMask.ColorBufferBit
+        this.SwapBuffers ()
+
+    override this.OnUpdateFrame args =
+        ()
