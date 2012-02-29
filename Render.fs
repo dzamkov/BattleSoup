84972b8fdb91c97ac12adffe719e6f56d21cf7f1
dzamkov <dzamkov@xiatek.org>
2012-02-29 14:38:39 -0500

    Circles!

--- Render.fs
+++ Render.fs
@@ -3,7 +3,61 @@
 open OpenTK
 open OpenTK.Graphics
 open OpenTK.Graphics.OpenGL
+open System.Drawing
+open System.Drawing.Imaging
 open BattleSoup.Geometry
+
+/// An interface to a OpenGL texture.
+type Texture (id : int) =
+
+    /// Gets the null texture.
+    static member Null = Texture 0
+
+    /// Creates and binds a 2d texture with no associated image data.
+    static member Create () =
+        let id = GL.GenTexture ()
+        GL.BindTexture (TextureTarget.Texture2D, id)
+        GL.TexEnv (TextureEnvTarget.TextureEnv, TextureEnvParameter.TextureEnvMode, int TextureEnvMode.Modulate);
+        Texture (id)
+
+    /// Creates and binds a 2d texture for the given bitmap.
+    static member Create (bitmap : Bitmap) =
+        let texture = Texture.Create ()
+        let width = bitmap.Width
+        let height = bitmap.Height
+        let area = System.Drawing.Rectangle (0, 0, width, height)
+        let bd = bitmap.LockBits (area, ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb)
+        GL.TexImage2D (TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, width, height, 0, PixelFormat.Bgra, PixelType.UnsignedByte, bd.Scan0)
+        bitmap.UnlockBits bd
+        texture
+
+    /// Creates and binds a 2d texture using the given drawing function.
+    static member Create (width : int, height : int, draw) =
+        use bitmap = new Bitmap (width, height)
+        use graphics = Graphics.FromImage bitmap
+        draw graphics
+        bitmap.Save "test.png"
+        Texture.Create bitmap
+
+    /// Sets the wrap mode for the currently-bound texture.
+    static member SetWrapMode (target, horizontal : TextureWrapMode, vertical : TextureWrapMode) =
+        GL.TexParameter (target, TextureParameterName.TextureWrapS, int horizontal)
+        GL.TexParameter (target, TextureParameterName.TextureWrapT, int vertical)
+
+    /// Sets the filter mode for the currently-bound texture.
+    static member SetFilterMode (target, min : TextureMinFilter, mag : TextureMagFilter) =
+        GL.TexParameter (target, TextureParameterName.TextureMinFilter, int min)
+        GL.TexParameter (target, TextureParameterName.TextureMagFilter, int mag)
+
+    /// Creates a mipmap for the currently-bound texture.
+    static member CreateMipmap (target) =
+        GL.Ext.GenerateMipmap target
+
+    /// Gets the ID for this texture.
+    member this.ID = id
+
+    /// Deletes this texture.
+    member this.Delete () = GL.DeleteTexture id
 
 /// Defines useful extension functions for the GL class.
 type GL with
@@ -12,6 +66,9 @@ type GL with
 
     static member TexCoord2 (point : Point) =
         GL.TexCoord2 (point.X, point.Y)
+
+    static member BindTexture2D (texture : Texture) =
+        GL.BindTexture (TextureTarget.Texture2D, texture.ID)
 
     static member MultMatrix (transform : Transform) =
         let a = transform.X.X
--- Window.fs
+++ Window.fs
@@ -3,6 +3,7 @@
 open OpenTK
 open OpenTK.Graphics
 open OpenTK.Graphics.OpenGL
+open System.Drawing
 open BattleSoup.World
 open BattleSoup.Geometry
 open BattleSoup.Render
@@ -12,6 +13,7 @@ type Window () =
     inherit GameWindow (640, 480, GraphicsMode.Default, "BattleSoup")
     let world = World ()
     let mutable view = Transform (Point (0.0, 0.0), Vector (10.0, 0.0), Vector (0.0, 10.0))
+    let mutable texture = Texture.Null
 
     override this.OnLoad args =
         for i = 0 to 9 do
@@ -24,21 +26,39 @@ type Window () =
                         Mass = 1.0
                         Radius = 0.5
                     }
+
+        this.MakeCurrent ()
+
+        GL.Enable EnableCap.Texture2D
+        GL.Enable EnableCap.ColorMaterial
         GL.Enable EnableCap.CullFace
+        GL.Enable EnableCap.Blend
+        GL.BlendFunc (BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha)
+        let draw width height (g : Graphics) =
+            g.Clear (Color.Transparent)
+            g.FillEllipse (Brushes.White, 0, 0, width, height)
+        texture <- Texture.Create (256, 256, draw 256 256)
+        Texture.SetFilterMode (TextureTarget.Texture2D, TextureMinFilter.Linear, TextureMagFilter.Linear)
+
 
     override this.OnRenderFrame args =
         GL.Clear ClearBufferMask.ColorBufferBit
         let view = normalizeView (float this.Width / float this.Height) view
 
+        GL.BindTexture2D texture
         GL.LoadIdentity ()
         GL.MultMatrix view.Inverse
         GL.Begin BeginMode.Quads
         for atom in world.Atoms do
             let position = atom.Position
             let radius = atom.Radius
+            GL.TexCoord2 (0.0, 0.0)
             GL.Vertex2 (position.X - radius, position.Y - radius)
+            GL.TexCoord2 (1.0, 0.0)
             GL.Vertex2 (position.X + radius, position.Y - radius)
+            GL.TexCoord2 (1.0, 1.0)
             GL.Vertex2 (position.X + radius, position.Y + radius)
+            GL.TexCoord2 (0.0, 1.0)
             GL.Vertex2 (position.X - radius, position.Y + radius)
         GL.End ()
 
